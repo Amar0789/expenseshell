1f075f31e63c000c5d573327466e7dbabcba7310
Amar <amarsaiyellanki789@gmail.com>
2025-04-17 10:44:29 +0530

    message

--- shell.sh
+++ shell.sh
@@ -1,9 +1,9 @@
 #! /bin/bash
 
-echo "Please enter your age "
+echo "Please enter your first number "
 read A
 
-echo "Please enter your lucky number"
+echo "Please enter your second number"
 read B
 
-echo "Hi, there your age is $A and your lucky number is $B"
+echo "The sum of two given numbers is $($A +$B)"
