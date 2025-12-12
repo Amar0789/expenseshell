f57fdaa613aa1159898562052e40119176c765ee
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 16:42:58 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -1,8 +1,3 @@
 #!/bin/bash/
-echo "These are variables"
-echo "Please enter your favourite number"
-read -s number
-if [ $number -gt 10 ]
-then echo "The number you have entered is greater than 10"
-else echo "The number you have entered is less than 10"
-fi
+fruits = ("Apple" "Banana" "Orange" "Grapes" "Mango" "Papaya")
+echo "My favourite is ${fruits[1]}"
