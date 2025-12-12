5570b630e15e86d78ddf04f3a9ecc718b6cd4487
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 16:59:14 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-echo "lets check addition"
-echo "Please enter your first number"
-read a
-echo "Please enter your second number"
-read b
-c=$(($a + $b))
-echo "The sum of your given 2 numbers is $c"
+echo "all arguments you have passed are $@"
