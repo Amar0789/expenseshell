a4188e02515232534f576f2f9b3d167fa3548a36
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 16:47:32 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -1,5 +1,5 @@
 #!/bin/bash/
-a=$1
 fruits=( "Apple" "Banana" "Orange" "Grapes" "Mango" "Papaya" )
 echo "Please enter a number in between 0-5"
+read a
 echo " The fruit you have selected is ${fruits[$a]}"
