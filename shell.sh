b352018a3bcf2f1a53c53040fbf6e30e7ff80050
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 17:03:44 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -2,3 +2,6 @@
 echo "All arguments you have passed are $@"
 echo "The number of arguments you have passed are $#"
 echo "The home directory of the user is $HOME"
+echo "The present workin directory of the user is $PWD"
+echo "The PID of present task is $$"
+echo "The PID of previous task is $!"
