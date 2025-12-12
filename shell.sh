21b63a776906503a6a648d29f2e02d25f8afd83d
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 16:40:42 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -2,4 +2,7 @@
 echo "These are variables"
 echo "Please enter your favourite number"
 read -s number
-echo "your favourite numner is $number"
+if [ $number -gt 10 ]
+then echo "The number you have entered is greater than 10"
+else echo "The number you have entered is less than 10"
+fi
