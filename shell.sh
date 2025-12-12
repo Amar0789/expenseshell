af72900f76f3c631ba63d6b42f2591ae4f9e7c65
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 17:45:36 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-echo "This is an example of for loop"
-for i in {2..100}
-do echo "Hi Amar Sai"
-done
+validate () {
+    if [ $1 -ne 0 ]
+    then echo "$2 is failed"
+    else echo "$2 is successul"
+    fi
+}
+
+dnf install nginx -y
+validate $? "Installing git"
