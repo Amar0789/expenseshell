089cabf1a4d596ceb7c6552917bbfbc253d72be1
Amar <amarsaiyellanki789@gmail.com>
2025-12-12 17:48:46 +0530

    This is a commit message

--- shell.sh
+++ shell.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-validate () {
-    if [ $1 -ne 0 ]
-    then echo "$2 is failed"
-    else echo "$2 is successul"
-    fi
-}
-
-dnf install nginx -y
-validate $? "Installing nginx"
+echo -e"\e[31m Hi"
