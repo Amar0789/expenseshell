9dd5fc5cefe5ee502d6431b425e2ec9eb4c9bada
Amar <amarsaiyellanki789@gmail.com>
2026-01-06 09:57:42 +0530

    dd

--- shell.sh
+++ shell.sh
@@ -1,11 +1,11 @@
 #!/bin/bash 
 set -ex
-trap '{LINENO}' ERR
+trap '{$LINENO}' ERR
 a=("apple" "banana" "carrot")
 echo "${a[1]}"
 ho=$(ls -la)
 echo "$ho"
-if [$? -eq 0]
+if [ $? -eq 0 ]
 then echo "its done"
 else echo "its not done"
 fi
