ee0377bafe904b9976cf5e774f59a09d14372ea6
Amar <amarsaiyellanki789@gmail.com>
2026-01-06 09:54:47 +0530

    dd

--- shell.sh
+++ shell.sh
@@ -1,10 +1,11 @@
 #!/bin/bash 
 set -ex
+trap '' ERR
 a=("apple" "banana" "carrot")
 echo "${a[1]}"
 ho=$(ls -la)
 echo "$ho"
-if [ $? -eq 0 ]
+if [$? -eq 0]
 then echo "its done"
 else echo "its not done"
 fi
